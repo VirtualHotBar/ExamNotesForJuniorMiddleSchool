2f5dbaaf70f3ec60d5cd004ee40c7c5dc4fd78ce
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2023-02-02 09:29:22 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -121,7 +121,7 @@ export default defineConfig({
       },
     ],
     footer: {
-      message: "Released under the MIT License.",
+      message: "<a href='https://beian.miit.gov.cn/' target='_blank' class='class-link' rel='noopener'>蜀ICP备2023001640号-2</a>",
       copyright: "Copyright © 2023-present VirtualHotBar",
     },
     editLink: {
